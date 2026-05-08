b8ca765bbd5451f6487310bc3ed0d067abf28fe0
Ilan Germano <ige22@uclive.ac.nz>
2026-05-09 01:18:30 +1200

    chore: first commit

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,12 @@
+export type LineItem = {
+  id: string;
+  description: string;
+  quantity: number;
+  unit_price: number;
+  total: number;
+  type: "labour" | "material";
+};
+
 export type Job = {
   id: string;
   title: string;
